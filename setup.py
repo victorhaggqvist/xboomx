068f4b504e0e0454af1a325939f9ac386016ba62
Victor Häggqvist <victor@snilius.com>
2014-09-02 22:29:48 +0200

    filter uniqe items and blacklist

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
+# -*- coding: utf-8 -*-
 from setuptools import setup
 
 
 setup(
     name='xboomx',
-    version='0.50',
+    version='0.60',
     packages=['xboomx'],
     scripts=['xboomx/bin/xboomx_path.py',
              'xboomx/bin/xboomx_sort.py',
@@ -11,12 +12,12 @@ setup(
              'xboomx/bin/xboomx_urls.py',
              'xboomx/bin/web_xboomx',
              'xboomx/bin/xboomx'],
-    license='BSD',
+    license='GPLv2',
     long_description='wrapper for most common occurences in dmenu',
     install_requires=[],
     include_package_data=True,
     package_data={'shared': ["etc/config"]},
-    author="Yuriy Netesov",
-    author_email="retnuhed@gmail.com",
-    url="https://bitbucket.org/dehun/xboomx/",
+    author="Victor Häggqvist",
+    author_email="victor@hggqvst.com",
+    url="https://github.com/victorhaggqvist/xboomx",
 )
--- xboomx/bin/xboomx_path.py
+++ xboomx/bin/xboomx_path.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python
 
 import os
+from xboomx.config import config
 
-pathes = os.environ['PATH'].split(':')
+def main():
+    pathes = os.environ['PATH'].split(':')
 
+    items = []
 
-for path in pathes:
-    if os.path.isdir(path):
-        for f in os.listdir(path):
-            print f
+    for path in pathes:
+        if os.path.isdir(path):
+            for f in os.listdir(path):
+                items.append(f)
+
+    uniqeitems = list(set(items))
+
+    ignorelist = config.get("ignorelist","");
+    for item in uniqeitems:
+        if item not in ignorelist:
+            print item
+
+main()
