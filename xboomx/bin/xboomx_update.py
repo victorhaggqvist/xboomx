15f677205a9b968d636703a5d49b99cf7bb84a15
Victor Häggqvist <victor@snilius.com>
2015-05-18 19:29:33 +0200

    handle Esc interrupt in dmenu

--- xboomx/bin/xboomx_update.py
+++ xboomx/bin/xboomx_update.py
@@ -12,24 +12,29 @@ def main():
     if len(sys.argv) > 1 and sys.argv[1] != "--stats":
         db_type = sys.argv[1]
 
-    item = fileinput.input()[0]
-    pprint(item)
-
-    item = item.strip('\n')
-
-    session = get_session()
     try:
-        dbitem = session.query(PathItem).filter_by(name=item).one()
-        dbitem.count = dbitem.count + 1
-        session.add(dbitem)
-    except NoResultFound:
-        dbi = PathItem(name=item, couunt=0)
-        session.add(dbi)
+        item = fileinput.input()[0]
+        pprint(item)
 
-    session.commit()
-    session.close()
+        item = item.strip('\n')
 
-    print(item)
+        session = get_session()
+        try:
+            dbitem = session.query(PathItem).filter_by(name=item).one()
+            dbitem.count = dbitem.count + 1
+            session.add(dbitem)
+        except NoResultFound:
+            dbi = PathItem(name=item, couunt=0)
+            session.add(dbi)
+
+        session.commit()
+        session.close()
+
+        print(item)
+    except IndexError:
+        # handle if one press Esc to exit dmenu, catch broken pipe
+        exit(0)
+
 
 if __name__ == '__main__':
     main()
